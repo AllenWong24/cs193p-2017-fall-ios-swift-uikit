2b12eca0648f9ce1bc9f024f949a224d647c9dfc
AllenWong <allenwong2407@outlook.com>
2023-03-25 16:35:15 +0800

    lecture_1

---
+++ lecture_1/Concentration/Concentration/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  Concentration
+//
+//  Created by Allen Wong on 2023/3/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var flipCount = 0 {
+        didSet {
+            flipCountLabel.text = "Flips: \(flipCount)"
+        }
+    }
+    
+    var emojiChoices = ["😎", "🥹", "😎", "🥹"]
+
+    @IBOutlet weak var flipCountLabel: UILabel!
+    
+    @IBOutlet var cardButtons: [UIButton]!
+    
+    @IBAction func touchCard(_ sender: UIButton) {
+        flipCount += 1
+        if let cardNumber = cardButtons.firstIndex(of: sender) {
+            flipCard(withEmoji: emojiChoices[cardNumber], on: sender)
+        }
+    }
+    
+    func flipCard(withEmoji emoji: String, on button: UIButton) {
+        if button.currentTitle == emoji {
+            button.setTitle("", for: .normal)
+            button.backgroundColor = .orange
+        } else {
+            button.setTitle(emoji, for: .normal)
+            button.backgroundColor = .white
+        }
+    }
+    
+}
+
