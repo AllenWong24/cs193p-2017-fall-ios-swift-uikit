d597e20030325708b52b588c99ca257cf0dcd654
AllenWong <allenwong2407@outlook.com>
2023-05-27 20:53:17 +0800

    lecture_14 document Untitled.json

--- lecture_14/EmojiArt/EmojiArt/EmojiArtDocument.swift
+++ lecture_14/EmojiArt/EmojiArt/EmojiArtDocument.swift
@@ -1,5 +1,5 @@
 //
-//  Document.swift
+//  EmojiArtDocument.swift
 //  EmojiArt
 //
 //  Created by Allen Wong on 2023/5/27.
@@ -7,15 +7,18 @@
 
 import UIKit
 
-class Document: UIDocument {
+class EmojiArtDocument: UIDocument {
+    
+    var emojiArt: EmojiArt?
     
     override func contents(forType typeName: String) throws -> Any {
-        // Encode your document with an instance of NSData or NSFileWrapper
-        return Data()
+        return emojiArt?.json ?? Data()
     }
     
     override func load(fromContents contents: Any, ofType typeName: String?) throws {
-        // Load your document from contents
+        if let json = contents as? Data {
+            emojiArt = EmojiArt(json: json)
+        }
     }
 }
 
---
+++ lecture_14/EmojiArt/EmojiArt/EmojiCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  EmojiCollectionViewCell.swift
+//  EmojiArt
+//
+//  Created by Allen Wong on 2023/5/13.
+//
+
+import UIKit
+
+class EmojiCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var label: UILabel!
+}
