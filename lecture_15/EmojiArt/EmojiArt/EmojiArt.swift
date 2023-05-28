e03c57cd21694632d6e3915950e7eacb5a802c6d
AllenWong <allenwong2407@outlook.com>
2023-05-28 14:28:41 +0800

    lecture_15

---
+++ lecture_15/EmojiArt/EmojiArt/EmojiArt.swift
@@ -0,0 +1,37 @@
+//
+//  EmojiArt.swift
+//  EmojiArt
+//
+//  Created by Allen Wong on 2023/5/27.
+//
+
+import Foundation
+
+struct EmojiArt: Codable {
+    var url: URL
+    var emojis = [EmojiInfo]()
+    
+    struct EmojiInfo: Codable {
+        let x: Int
+        let y: Int
+        let text: String
+        let size: Int
+    }
+    
+    init?(json: Data) {
+        if let newValue = try? JSONDecoder().decode(EmojiArt.self, from: json) {
+            self = newValue
+        } else {
+            return nil
+        }
+    }
+    
+    var json: Data? {
+        return try? JSONEncoder().encode(self)
+    }
+    
+    init(url: URL, emojis: [EmojiInfo] = [EmojiInfo]()) {
+        self.url = url
+        self.emojis = emojis
+    }
+}
