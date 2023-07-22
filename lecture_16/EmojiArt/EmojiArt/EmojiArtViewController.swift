b28c8b1099057f0ed1a49754b80339ffb6c25ba5
AllenWong <allenwong2407@outlook.com>
2023-07-22 16:57:02 +0800

    lecture_16 Unwind Segue

--- lecture_16/EmojiArt/EmojiArt/EmojiArtViewController.swift
+++ lecture_16/EmojiArt/EmojiArt/EmojiArtViewController.swift
@@ -52,6 +52,10 @@ class EmojiArtViewController: UIViewController, UIDropInteractionDelegate, UIScr
         return .none
     }
     
+    @IBAction func close(bySegue: UIStoryboardSegue) {
+        close()
+    }
+    
     // MARK: - model
     
     // computed property for our Model
@@ -102,7 +106,7 @@ class EmojiArtViewController: UIViewController, UIDropInteractionDelegate, UIScr
         }
     }
     
-    @IBAction func close(_ sender: UIBarButtonItem) {
+    @IBAction func close(_ sender: UIBarButtonItem? = nil) {
         if let observer = emojiArtViewObserver {
             NotificationCenter.default.removeObserver(observer)
         }
